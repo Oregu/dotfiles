9185f7490e93b42f7af7a8d4be7793c4a82814e6
Oregu <oleg.burykin@gmail.com>
2014-05-17 17:13:04 +0400

    xmonad for idea

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,19 +1,27 @@
 import XMonad
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.FadeInactive
+import XMonad.Hooks.SetWMName
+
 import XMonad.Util.EZConfig
 import Graphics.X11.ExtraTypes.XF86
 
 main :: IO ()
 main = xmonad $ defaultConfig
-        { manageHook = manageDocks <+> manageHook defaultConfig
-        , logHook    = fadeHook
+        { manageHook = manageDocks <+> manageHook'
+        , logHook    = logHook'
         , layoutHook = avoidStruts  $  layoutHook defaultConfig
-        , modMask    = mod4Mask     -- Rebind Mod to the Windows key
+        , startupHook = setWMName "LG3D" -- For Swing
+        , normalBorderColor  = normalBorderColor'
+        , focusedBorderColor = focusedBorderColor'
+        , modMask    = mod4Mask          -- Mod as the Windows key
         } `additionalKeys` keys'
 
-fadeHook :: X ()
-fadeHook = fadeInactiveLogHook fadeAmount
+manageHook' :: ManageHook
+manageHook' = manageHook defaultConfig
+
+logHook' :: X ()
+logHook' = fadeInactiveLogHook fadeAmount
     where fadeAmount = 0.8
 
 keys' :: [((KeyMask, KeySym), X ())]
@@ -21,8 +29,15 @@ keys' =
     [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock'")
     , ((controlMask, xK_Print), spawn "sleep 0.2; scrot -s -e 'mv $f ~/Pictures")
     , ((0, xK_Print), spawn "scrot -e 'mv $f ~/Pictures'")
+    , ((0, xF86XK_MonBrightnessDown),  spawn "xbacklight -20")
     , ((0, xF86XK_MonBrightnessUp),  spawn "xbacklight +20")
     , ((0, xF86XK_AudioMute),        spawn "amixer set Master toggle && amixer set Headphone toggle")
     , ((0, xF86XK_AudioRaiseVolume), spawn "amixer set Master on && amixer set Headphone on && amixer set Master 2+")
     , ((0, xF86XK_AudioLowerVolume), spawn "amixer set Master on && amixer set Headphone on && amixer set Master 2-")
     ]
+
+normalBorderColor' :: String
+normalBorderColor' = "gray"
+
+focusedBorderColor' :: String
+focusedBorderColor' = "purple"
